8bc1f21dd7826bd6bb553038ff8a2bb8cb69c953
Sylvain Wallez <sylvain@elastic.co>
2022-01-24 16:41:03 +0100

    Add support for typed-key arrays, refactor and add tests (#125)

---
+++ java-client/src/test/java/co/elastic/clients/elasticsearch/ElasticsearchTestServer.java
@@ -0,0 +1,111 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package co.elastic.clients.elasticsearch;
+
+import co.elastic.clients.json.JsonpMapper;
+import co.elastic.clients.json.jsonb.JsonbJsonpMapper;
+import co.elastic.clients.transport.ElasticsearchTransport;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.elasticsearch.client.RestClient;
+import org.testcontainers.elasticsearch.ElasticsearchContainer;
+
+import java.time.Duration;
+
+public class ElasticsearchTestServer implements AutoCloseable {
+
+    private volatile ElasticsearchContainer container;
+    private int port;
+    private final JsonpMapper mapper = new JsonbJsonpMapper();
+    private RestClient restClient;
+    private ElasticsearchTransport transport;
+    private ElasticsearchClient client;
+
+    private static ElasticsearchTestServer global;
+
+    public static synchronized ElasticsearchTestServer global() {
+        if (global == null) {
+            System.out.println("Starting global ES test server.");
+            global = new ElasticsearchTestServer();
+            global.setup();
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                System.out.println("Stopping global ES test server.");
+                global.close();
+            }));
+        }
+        return global;
+    }
+
+    private synchronized void setup() {
+        container = new ElasticsearchContainer("docker.elastic.co/elasticsearch/elasticsearch:7.16.2")
+            .withEnv("ES_JAVA_OPTS", "-Xms256m -Xmx256m")
+            .withEnv("path.repo", "/tmp") // for snapshots
+            .withStartupTimeout(Duration.ofSeconds(30))
+            .withPassword("changeme");
+        container.start();
+        port = container.getMappedPort(9200);
+
+        BasicCredentialsProvider credsProv = new BasicCredentialsProvider();
+        credsProv.setCredentials(
+            AuthScope.ANY, new UsernamePasswordCredentials("elastic", "changeme")
+        );
+        restClient = RestClient.builder(new HttpHost("localhost", port))
+            .setHttpClientConfigCallback(hc -> hc.setDefaultCredentialsProvider(credsProv))
+            .build();
+        transport = new RestClientTransport(restClient, mapper);
+        client = new ElasticsearchClient(transport);
+    }
+
+    @Override
+    public void close() {
+        if (this == global) {
+            // Closed with a shutdown hook
+            return;
+        }
+
+        if (container != null) {
+            container.stop();
+        }
+        container = null;
+    }
+
+    public int port() {
+        return port;
+    }
+
+    public RestClient restClient() {
+        return restClient;
+    }
+
+    public ElasticsearchTransport transport() {
+        return transport;
+    }
+
+    public JsonpMapper mapper() {
+        return mapper;
+    }
+
+    public ElasticsearchClient client() {
+        return client;
+    }
+}
