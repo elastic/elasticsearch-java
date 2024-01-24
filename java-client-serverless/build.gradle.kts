1974578a450d6c08c69d4a765bce2c160afe3655
Sylvain Wallez <sylvain@elastic.co>
2024-01-24 13:48:46 +0100

    Upgrade Parsson from 1.0.0 to 1.0.5 (#722) (#731)
    fixes #705
    
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>
    Co-authored-by: Aurélien Pupier <apupier@redhat.com>

--- java-client-serverless/build.gradle.kts
+++ java-client-serverless/build.gradle.kts
@@ -210,7 +210,7 @@ dependencies {
     // Needed even if using Jackson to have an implementation of the Jsonp object model
     // EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
     // https://github.com/eclipse-ee4j/parsson
-    api("org.eclipse.parsson:parsson:1.0.0")
+    api("org.eclipse.parsson:parsson:1.0.5")
 
     // OpenTelemetry API for native instrumentation of the client.
     // Apache 2.0
