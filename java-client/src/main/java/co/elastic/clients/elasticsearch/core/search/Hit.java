36f27a2add3b3e466d758aa9e8229ba56a373216
Laura Trotta <laura.trotta@elastic.co>
2024-05-30 10:28:54 +0200

    [codegen] update to latest api spec

--- java-client/src/main/java/co/elastic/clients/elasticsearch/core/search/Hit.java
+++ java-client/src/main/java/co/elastic/clients/elasticsearch/core/search/Hit.java
@@ -72,6 +72,7 @@ import javax.annotation.Nullable;
 public class Hit<TDocument> implements JsonpSerializable {
 	private final String index;
 
+	@Nullable
 	private final String id;
 
 	@Nullable
@@ -127,7 +128,7 @@ public class Hit<TDocument> implements JsonpSerializable {
 	private Hit(Builder<TDocument> builder) {
 
 		this.index = ApiTypeHelper.requireNonNull(builder.index, this, "index");
-		this.id = ApiTypeHelper.requireNonNull(builder.id, this, "id");
+		this.id = builder.id;
 		this.score = builder.score;
 		this.type = builder.type;
 		this.explanation = builder.explanation;
@@ -161,8 +162,9 @@ public class Hit<TDocument> implements JsonpSerializable {
 	}
 
 	/**
-	 * Required - API name: {@code _id}
+	 * API name: {@code _id}
 	 */
+	@Nullable
 	public final String id() {
 		return this.id;
 	}
@@ -311,9 +313,11 @@ public class Hit<TDocument> implements JsonpSerializable {
 		generator.writeKey("_index");
 		generator.write(this.index);
 
-		generator.writeKey("_id");
-		generator.write(this.id);
+		if (this.id != null) {
+			generator.writeKey("_id");
+			generator.write(this.id);
 
+		}
 		if (this.score != null) {
 			generator.writeKey("_score");
 			generator.write(this.score);
@@ -458,6 +462,7 @@ public class Hit<TDocument> implements JsonpSerializable {
 				ObjectBuilder<Hit<TDocument>> {
 		private String index;
 
+		@Nullable
 		private String id;
 
 		@Nullable
@@ -523,9 +528,9 @@ public class Hit<TDocument> implements JsonpSerializable {
 		}
 
 		/**
-		 * Required - API name: {@code _id}
+		 * API name: {@code _id}
 		 */
-		public final Builder<TDocument> id(String value) {
+		public final Builder<TDocument> id(@Nullable String value) {
 			this.id = value;
 			return this;
 		}
