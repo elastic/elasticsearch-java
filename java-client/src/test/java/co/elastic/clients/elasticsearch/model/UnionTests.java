ae3442c5182a389932e648e65c4a5b39bec545d7
Laura Trotta <laura.trotta@elastic.co>
2024-07-12 10:28:08 +0200

    code style

--- java-client/src/test/java/co/elastic/clients/elasticsearch/model/UnionTests.java
+++ java-client/src/test/java/co/elastic/clients/elasticsearch/model/UnionTests.java
@@ -25,21 +25,12 @@ import co.elastic.clients.elasticsearch._types.Script;
 import co.elastic.clients.elasticsearch._types.Time;
 import co.elastic.clients.elasticsearch._types.aggregations.Aggregate;
 import co.elastic.clients.elasticsearch._types.mapping.Property;
-import co.elastic.clients.elasticsearch._types.query_dsl.DecayFunction;
 import co.elastic.clients.elasticsearch._types.query_dsl.DistanceFeatureQuery;
-import co.elastic.clients.elasticsearch._types.query_dsl.FunctionScore;
-import co.elastic.clients.elasticsearch._types.query_dsl.FunctionScoreBuilders;
-import co.elastic.clients.elasticsearch._types.query_dsl.FunctionScoreQuery;
 import co.elastic.clients.elasticsearch._types.query_dsl.MultiValueMode;
 import co.elastic.clients.elasticsearch._types.query_dsl.Query;
-import co.elastic.clients.elasticsearch._types.query_dsl.RangeQuery;
 import co.elastic.clients.elasticsearch._types.query_dsl.RangeRelation;
 import co.elastic.clients.elasticsearch._types.query_dsl.TermRangeQuery;
-import co.elastic.clients.elasticsearch._types.query_dsl.UntypedRangeQuery;
-import co.elastic.clients.elasticsearch.core.SearchRequest;
 import co.elastic.clients.elasticsearch.core.SearchResponse;
-import co.elastic.clients.json.JsonData;
-import co.elastic.clients.json.JsonpUtils;
 import co.elastic.clients.testkit.ModelTestCase;
 import jakarta.json.Json;
 import jakarta.json.JsonObject;
@@ -120,7 +111,7 @@ public class UnionTests extends ModelTestCase {
     public void testOpenTypedKeysUnion() {
         String requiredFields =
             "{\"took\":1,\"timed_out\":false,\"_shards\":{\"failed\":0.0,\"successful\":1.0,\"total\":1.0}," +
-            "\"hits\":{\"total\":{\"relation\":\"eq\",\"value\":0},\"hits\":[]}}";
+                "\"hits\":{\"total\":{\"relation\":\"eq\",\"value\":0},\"hits\":[]}}";
 
         String customAgg = "{\"bar\":42}";
 
@@ -152,16 +143,17 @@ public class UnionTests extends ModelTestCase {
     public void testUntaggedUnion() {
         // Range queries
         Query numberRangeQuery = Query.of(q -> q.range(r -> r
-                .number(n -> n
-                    .field("age")
-                    .gte(10D)
-                    .lte(20D)
-                    .boost(1.0F)
-                    .relation(RangeRelation.Contains))));
+            .number(n -> n
+                .field("age")
+                .gte(10D)
+                .lte(20D)
+                .boost(1.0F)
+                .relation(RangeRelation.Contains))));
 
-        String expectedNumberRangeQuery = "{\"range\":{\"age\":{\"boost\":1.0,\"relation\":\"contains\",\"gte\":10.0,\"lte\":20.0}}}";
+        String expectedNumberRangeQuery = "{\"range\":{\"age\":{\"boost\":1.0,\"relation\":\"contains\"," +
+            "\"gte\":10.0,\"lte\":20.0}}}";
 
-        assertEquals(expectedNumberRangeQuery,toJson(numberRangeQuery));
+        assertEquals(expectedNumberRangeQuery, toJson(numberRangeQuery));
 
         Query dateRangeQuery = Query.of(q -> q.range(r -> r
             .date(d -> d
@@ -172,9 +164,10 @@ public class UnionTests extends ModelTestCase {
                 .format("some-format")
                 .relation(RangeRelation.Contains))));
 
-        String expectedDateRangeQuery = "{\"range\":{\"day\":{\"boost\":1.0,\"relation\":\"contains\",\"gte\":\"20-06-24\",\"lte\":\"20-07-24\"},\"format\":\"some-format\"}}";
+        String expectedDateRangeQuery = "{\"range\":{\"day\":{\"boost\":1.0,\"relation\":\"contains\"," +
+            "\"gte\":\"20-06-24\",\"lte\":\"20-07-24\"},\"format\":\"some-format\"}}";
 
-        assertEquals(expectedDateRangeQuery,toJson(dateRangeQuery));
+        assertEquals(expectedDateRangeQuery, toJson(dateRangeQuery));
 
         Query termRangeQuery = Query.of(q -> q.range(r -> r
             .term(d -> d
@@ -184,12 +177,13 @@ public class UnionTests extends ModelTestCase {
                 .boost(1.0F)
                 .relation(RangeRelation.Contains))));
 
-        String expectedTermRangeQuery = "{\"range\":{\"word\":{\"boost\":1.0,\"relation\":\"contains\",\"gte\":\"some\",\"lte\":\"word\"}}}";
+        String expectedTermRangeQuery = "{\"range\":{\"word\":{\"boost\":1.0,\"relation\":\"contains\"," +
+            "\"gte\":\"some\",\"lte\":\"word\"}}}";
 
-        assertEquals(expectedTermRangeQuery,toJson(termRangeQuery));
+        assertEquals(expectedTermRangeQuery, toJson(termRangeQuery));
 
         String jsonNumberRangeQuery = "{\n" +
-        "    \"range\": {\n" +
+            "    \"range\": {\n" +
             "      \"age\": {\n" +
             "        \"gte\": 10,\n" +
             "        \"lte\": 20,\n" +
@@ -206,7 +200,8 @@ public class UnionTests extends ModelTestCase {
         assertEquals(20, numberRangeQueryFromJson.range().untyped().lte().to(Integer.class));
         assertEquals(RangeRelation.Contains, numberRangeQueryFromJson.range().untyped().relation());
 
-        Query converted = TermRangeQuery.of(trq -> trq.field("a").gte("b").lte("c"))._toRangeQuery()._toQuery();
+        Query converted =
+            TermRangeQuery.of(trq -> trq.field("a").gte("b").lte("c"))._toRangeQuery()._toQuery();
         assertTrue(converted.isRange());
 
         // Distance feature queries
@@ -219,8 +214,10 @@ public class UnionTests extends ModelTestCase {
                     .pivot("1000m")
                     .origin(GeoLocation.of(gl -> gl.latlon(LatLonGeoLocation.of(ll -> ll.lon(-71.3).lat(41.15))))))))));
 
-        String expectedGeoDistFeatQuery = "{\"bool\":{\"must\":[{\"match_all\":{}}],\"should\":[{\"distance_feature\":{\"origin\":{\"lat\":41.15,\"lon\":-71.3},\"pivot\":\"1000m\",\"field\":\"location\"}}]}}";
-        assertEquals(expectedGeoDistFeatQuery,toJson(geoDistanceFeatureQuery));
+        String expectedGeoDistFeatQuery = "{\"bool\":{\"must\":[{\"match_all\":{}}]," +
+            "\"should\":[{\"distance_feature\":{\"origin\":{\"lat\":41.15,\"lon\":-71.3}," +
+            "\"pivot\":\"1000m\",\"field\":\"location\"}}]}}";
+        assertEquals(expectedGeoDistFeatQuery, toJson(geoDistanceFeatureQuery));
 
 
         Query dateDistanceFeatureQuery = Query.of(q -> q.bool(b -> b
@@ -231,8 +228,9 @@ public class UnionTests extends ModelTestCase {
                     .pivot(Time.of(t -> t.time("7d")))
                     .origin("now"))))));
 
-        String expectedDateDistFeatQuery = "{\"bool\":{\"must\":[{\"match_all\":{}}],\"should\":[{\"distance_feature\":{\"origin\":\"now\",\"pivot\":\"7d\",\"field\":\"date\"}}]}}";
-        assertEquals(expectedDateDistFeatQuery,toJson(dateDistanceFeatureQuery));
+        String expectedDateDistFeatQuery = "{\"bool\":{\"must\":[{\"match_all\":{}}]," +
+            "\"should\":[{\"distance_feature\":{\"origin\":\"now\",\"pivot\":\"7d\",\"field\":\"date\"}}]}}";
+        assertEquals(expectedDateDistFeatQuery, toJson(dateDistanceFeatureQuery));
 
         String distDateQuery = "{\n" +
             "    \"bool\": {\n" +
@@ -251,8 +249,10 @@ public class UnionTests extends ModelTestCase {
 
         Query distDateQueryFromJson = Query.of(q -> q.withJson(new StringReader(distDateQuery)));
         assertTrue(distDateQueryFromJson.bool().should().get(0).distanceFeature().isUntyped());
-        assertEquals("7d",distDateQueryFromJson.bool().should().get(0).distanceFeature().untyped().pivot().to(String.class));
-        assertEquals("date",distDateQueryFromJson.bool().should().get(0).distanceFeature().untyped().field().toString());
+        assertEquals("7d",
+            distDateQueryFromJson.bool().should().get(0).distanceFeature().untyped().pivot().to(String.class));
+        assertEquals("date",
+            distDateQueryFromJson.bool().should().get(0).distanceFeature().untyped().field().toString());
 
 
         String distGeoQuery = "{\n" +
@@ -272,8 +272,10 @@ public class UnionTests extends ModelTestCase {
 
         Query distGeoQueryFromJson = Query.of(q -> q.withJson(new StringReader(distGeoQuery)));
         assertTrue(distGeoQueryFromJson.bool().should().get(0).distanceFeature().isUntyped());
-        assertEquals("1000m",distGeoQueryFromJson.bool().should().get(0).distanceFeature().untyped().pivot().to(String.class));
-        assertEquals("location", distGeoQueryFromJson.bool().should().get(0).distanceFeature().untyped().field());
+        assertEquals("1000m",
+            distGeoQueryFromJson.bool().should().get(0).distanceFeature().untyped().pivot().to(String.class));
+        assertEquals("location",
+            distGeoQueryFromJson.bool().should().get(0).distanceFeature().untyped().field());
 
 
         Query convertedDist = DistanceFeatureQuery.of(dfq -> dfq.date(d -> d
@@ -289,13 +291,14 @@ public class UnionTests extends ModelTestCase {
             .functions(fn -> fn
                 .gauss(g -> g
                     .date(d -> d.field("date").placement(pl -> pl
-                    .origin("2013-09-17")
-                    .scale(Time.of(t -> t.time("10d")))
-                    .offset(Time.of(t -> t.time("5d")))
-                    .decay(0.5D)))))));
+                        .origin("2013-09-17")
+                        .scale(Time.of(t -> t.time("10d")))
+                        .offset(Time.of(t -> t.time("5d")))
+                        .decay(0.5D)))))));
 
-        String expectedFunctionScoreQueryGaussDate = "{\"function_score\":{\"functions\":[{\"gauss\":{\"date\":{\"decay\":0.5,\"offset\":\"5d\",\"scale\":\"10d\",\"origin\":\"2013-09-17\"}}}]}}";
-        assertEquals(expectedFunctionScoreQueryGaussDate,toJson(functionScoreQueryGaussDate));
+        String expectedFunctionScoreQueryGaussDate = "{\"function_score\":{\"functions\":[{\"gauss" +
+            "\":{\"date\":{\"decay\":0.5,\"offset\":\"5d\",\"scale\":\"10d\",\"origin\":\"2013-09-17\"}}}]}}";
+        assertEquals(expectedFunctionScoreQueryGaussDate, toJson(functionScoreQueryGaussDate));
 
         Query functionScoreQueryExpGeo = Query.of(q -> q.functionScore(fs -> fs
             .functions(fn -> fn
@@ -306,8 +309,10 @@ public class UnionTests extends ModelTestCase {
                         .offset("2km")
                         .decay(0.5D)))))));
 
-        String expectedFunctionScoreQueryExpGeo = "{\"function_score\":{\"functions\":[{\"exp\":{\"location\":{\"decay\":0.5,\"offset\":\"2km\",\"scale\":\"1km\",\"origin\":{\"lat\":41.15,\"lon\":-71.3}}}}]}}";
-        assertEquals(expectedFunctionScoreQueryExpGeo,toJson(functionScoreQueryExpGeo));
+        String expectedFunctionScoreQueryExpGeo = "{\"function_score\":{\"functions\":[{\"exp\":{\"location" +
+            "\":{\"decay\":0.5,\"offset\":\"2km\",\"scale\":\"1km\",\"origin\":{\"lat\":41.15,\"lon\":-71" +
+            ".3}}}}]}}";
+        assertEquals(expectedFunctionScoreQueryExpGeo, toJson(functionScoreQueryExpGeo));
 
         Query functionScoreQueryLinearNumeric = Query.of(q -> q.functionScore(fs -> fs
             .functions(fn -> fn
@@ -318,8 +323,9 @@ public class UnionTests extends ModelTestCase {
                         .offset(4D)
                         .decay(0.5D)))))));
 
-        String expectedFunctionScoreQueryLinearNumeric = "{\"function_score\":{\"functions\":[{\"linear\":{\"nums\":{\"decay\":0.5,\"offset\":4.0,\"scale\":3.0,\"origin\":2.0}}}]}}";
-        assertEquals(expectedFunctionScoreQueryLinearNumeric,toJson(functionScoreQueryLinearNumeric));
+        String expectedFunctionScoreQueryLinearNumeric = "{\"function_score\":{\"functions\":[{\"linear" +
+            "\":{\"nums\":{\"decay\":0.5,\"offset\":4.0,\"scale\":3.0,\"origin\":2.0}}}]}}";
+        assertEquals(expectedFunctionScoreQueryLinearNumeric, toJson(functionScoreQueryLinearNumeric));
 
         String funScoreQuery =
             " {\n" +
@@ -342,8 +348,10 @@ public class UnionTests extends ModelTestCase {
 
         Query funScoreQueryFromJson = Query.of(q -> q.withJson(new StringReader(funScoreQuery)));
         assertTrue(funScoreQueryFromJson.functionScore().functions().get(0).gauss().isUntyped());
-        assertEquals("2013-09-17", funScoreQueryFromJson.functionScore().functions().get(0).gauss().untyped().placement().origin().to(String.class));
-        assertEquals(MultiValueMode.Avg, funScoreQueryFromJson.functionScore().functions().get(0).gauss().untyped().multiValueMode());
+        assertEquals("2013-09-17",
+            funScoreQueryFromJson.functionScore().functions().get(0).gauss().untyped().placement().origin().to(String.class));
+        assertEquals(MultiValueMode.Avg,
+            funScoreQueryFromJson.functionScore().functions().get(0).gauss().untyped().multiValueMode());
 
 
     }
