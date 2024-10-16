30ee62651243a528169be3279396a5a773307e75
Laura Trotta <153528055+l-trotta@users.noreply.github.com>
2024-10-16 12:42:51 +0200

    Update java-client/src/main/java/co/elastic/clients/transport/TransportOptions.java
    Co-authored-by: Sylvain Wallez <sylvain@elastic.co>

--- java-client/src/main/java/co/elastic/clients/transport/TransportOptions.java
+++ java-client/src/main/java/co/elastic/clients/transport/TransportOptions.java
@@ -38,6 +38,11 @@ public interface TransportOptions {
 
     Function<List<String>, Boolean> onWarnings();
 
+    /**
+     * If {@code true}, the response body in {@code TransportException.response().body()} is guaranteed to be
+     * replayable (i.e. buffered), even if the original response was streamed. This allows inspecting the
+     * response body in case of error.
+     */
     boolean keepResponseBodyOnException();
 
     Builder toBuilder();
