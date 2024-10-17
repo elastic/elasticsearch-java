d9cb8c51f1f4fb58806197b806078a1cd68f6774
Laura Trotta <153528055+l-trotta@users.noreply.github.com>
2024-10-17 17:46:49 +0200

    Feature: option to retrieve original json body if parse exception occurred (#886)
    * base working impl
    
    * twr, remove unused constructor
    
    * unit test
    
    * license
    
    * complete refactor
    
    * reverting old changes
    
    * codestyle
    
    * leave ElasticsearchTransportBase untouched
    
    * Update java-client/src/main/java/co/elastic/clients/transport/TransportOptions.java
    
    Co-authored-by: Sylvain Wallez <sylvain@elastic.co>
    
    * Update java-client/src/main/java/co/elastic/clients/transport/TransportOptions.java
    
    Co-authored-by: Sylvain Wallez <sylvain@elastic.co>
    
    * custom generic response, checking in transport base
    
    * license header
    
    * asciidocs
    
    ---------
    
    Co-authored-by: Sylvain Wallez <sylvain@elastic.co>

--- java-client/src/test/java/co/elastic/clients/transport/TransportTest.java
+++ java-client/src/test/java/co/elastic/clients/transport/TransportTest.java
@@ -21,24 +21,34 @@ package co.elastic.clients.transport;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.http.RepeatableBodyResponse;
+import co.elastic.clients.transport.rest_client.RestClientOptions;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
+import co.elastic.clients.util.BinaryData;
 import com.sun.net.httpserver.HttpServer;
 import org.apache.http.HttpHost;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.Response;
 import org.elasticsearch.client.RestClient;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+
+import static co.elastic.clients.util.ContentType.APPLICATION_JSON;
 
 public class TransportTest extends Assertions {
 
     @Test
     public void testXMLResponse() throws Exception {
-        HttpServer httpServer = HttpServer.create(new InetSocketAddress(InetAddress.getLoopbackAddress(), 0), 0);
+        HttpServer httpServer = HttpServer.create(new InetSocketAddress(InetAddress.getLoopbackAddress(),
+            0), 0);
 
         httpServer.createContext("/_cat/indices", exchange -> {
             exchange.sendResponseHeaders(401, 0);
@@ -56,7 +66,8 @@ public class TransportTest extends Assertions {
             .builder(new HttpHost(address.getHostString(), address.getPort(), "http"))
             .build();
 
-        ElasticsearchClient esClient = new ElasticsearchClient(new RestClientTransport(restClient, new JacksonJsonpMapper()));
+        ElasticsearchClient esClient = new ElasticsearchClient(new RestClientTransport(restClient,
+            new JacksonJsonpMapper()));
 
         TransportException ex = Assertions.assertThrows(
             TransportException.class,
@@ -69,7 +80,76 @@ public class TransportTest extends Assertions {
         assertEquals("es/cat.indices", ex.endpointId());
 
         // Original response is transport-dependent
-        Response restClientResponse = (Response)ex.response().originalResponse();
+        Response restClientResponse = (Response) ex.response().originalResponse();
         assertEquals(401, restClientResponse.getStatusLine().getStatusCode());
+    }
+
+
+    @Test
+    public void testOriginalJsonBodyRetrievalException() throws Exception {
+        HttpServer httpServer = HttpServer.create(new InetSocketAddress(InetAddress.getLoopbackAddress(),
+            0), 0);
+
+        httpServer.createContext("/_cat/indices", exchange -> {
+            exchange.getResponseHeaders().put("Content-Type", Collections.singletonList(APPLICATION_JSON));
+            exchange.getResponseHeaders().put("X-Elastic-Product", Collections.singletonList("Elasticsearch"
+            ));
+            exchange.sendResponseHeaders(200, 0);
+            OutputStream out = exchange.getResponseBody();
+            out.write(
+                "definitely not json".getBytes(StandardCharsets.UTF_8)
+            );
+            out.close();
+        });
+
+        httpServer.start();
+        InetSocketAddress address = httpServer.getAddress();
+
+        RestClient restClient = RestClient
+            .builder(new HttpHost(address.getHostString(), address.getPort(), "http"))
+            .build();
+
+        // no transport options, response is not RepeatableBodyResponse, original body cannot be retrieved
+        ElasticsearchClient esClient = new ElasticsearchClient(new RestClientTransport(restClient,
+            new JacksonJsonpMapper()));
+
+        TransportException ex = Assertions.assertThrows(
+            TransportException.class,
+            () -> esClient.cat().indices()
+        );
+
+        assertEquals(200, ex.statusCode());
+        assertNotEquals(RepeatableBodyResponse.class, ex.response().getClass());
+
+        // setting transport option
+        RestClientOptions options = new RestClientOptions(RequestOptions.DEFAULT, true);
+
+        ElasticsearchTransport transport = new RestClientTransport(
+            restClient, new JacksonJsonpMapper(), options);
+
+        ElasticsearchClient esClientOptions = new ElasticsearchClient(transport);
+
+        ex = Assertions.assertThrows(
+            TransportException.class,
+            () -> esClientOptions.cat().indices()
+        );
+
+        httpServer.stop(0);
+
+        assertEquals(200, ex.statusCode());
+        assertEquals(RepeatableBodyResponse.class, ex.response().getClass());
+
+        try (RepeatableBodyResponse repeatableResponse = (RepeatableBodyResponse) ex.response()){
+            BinaryData body = repeatableResponse.body();
+                StringBuilder sb = new StringBuilder();
+                BufferedReader br = new BufferedReader(new InputStreamReader(body.asInputStream()));
+                String read;
+
+                while ((read = br.readLine()) != null) {
+                    sb.append(read);
+                }
+                br.close();
+            assertEquals("definitely not json",sb.toString());
+        }
     }
 }
