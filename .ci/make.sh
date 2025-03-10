ee7d86f1874b7bd47c934c052d57d93ae649e5f8
Laura Trotta <laura.trotta@elastic.co>
2025-03-10 16:15:24 +0100

    debug options

--- .ci/make.sh
+++ .ci/make.sh
@@ -202,7 +202,7 @@ if [[ "$CMD" == "release" ]]; then
     $git_mount $src_mount $output_mount \
     -v /tmp/secured:/tmp/secured \
     $docker_image \
-    $gradle_task
+    $gradle_task --scan --stacktrace --debug
 fi
 
 if [[ "$CMD" == "bump" ]]; then
