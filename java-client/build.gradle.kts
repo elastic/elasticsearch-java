b3451d797cf6dc6bc430d761205146d115843909
Laura Trotta <laura.trotta@elastic.co>
2025-11-03 11:14:51 +0100

    update inner es dependency

--- java-client/build.gradle.kts
+++ java-client/build.gradle.kts
@@ -175,7 +175,7 @@ signing {
 }
 
 dependencies {
-    val elasticsearchVersion = "9.0.0"
+    val elasticsearchVersion = "9.2.0"
     val jacksonVersion = "2.18.3"
     val jackson3Version = "3.0.0"
     val openTelemetryVersion = "1.32.0"
