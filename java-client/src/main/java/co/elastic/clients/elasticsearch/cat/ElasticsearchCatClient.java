ce198d8365e8f856b5eec149efab3ec58487a587
Laura Trotta <laura.trotta@elastic.co>
2026-04-07 12:51:52 +0000

    [codegen] update to latest spec

--- java-client/src/main/java/co/elastic/clients/elasticsearch/cat/ElasticsearchCatClient.java
+++ java-client/src/main/java/co/elastic/clients/elasticsearch/cat/ElasticsearchCatClient.java
@@ -525,6 +525,17 @@ public class ElasticsearchCatClient extends ApiClient<ElasticsearchTransport, El
 	 * include hidden nested documents. To get an accurate count of Elasticsearch
 	 * documents, use the cat count or count APIs.
 	 * <p>
+	 * NOTE: Storage metrics reported by this API reflect the post-compression size
+	 * of the indices on disk. Because these values are calculated after
+	 * Elasticsearch compresses the data and processes deletions, they are typically
+	 * significantly smaller than the raw, uncompressed data volume ingested.
+	 * <p>
+	 * IMPORTANT: For Elastic Cloud Serverless, ingest billing is based on the raw,
+	 * uncompressed data volume, not the post-compression metrics reported here. To
+	 * learn more, refer to <a href=
+	 * "https://www.elastic.co/docs/deploy-manage/cloud-organization/billing/elasticsearch-billing-dimensions">Elasticsearch
+	 * billing dimensions</a>.
+	 * <p>
 	 * CAT APIs are only intended for human consumption using the command line or
 	 * Kibana console. They are not intended for use by applications. For
 	 * application consumption, use an index endpoint.
@@ -561,6 +572,17 @@ public class ElasticsearchCatClient extends ApiClient<ElasticsearchTransport, El
 	 * internally to power indexing and search. As a result, all document counts
 	 * include hidden nested documents. To get an accurate count of Elasticsearch
 	 * documents, use the cat count or count APIs.
+	 * <p>
+	 * NOTE: Storage metrics reported by this API reflect the post-compression size
+	 * of the indices on disk. Because these values are calculated after
+	 * Elasticsearch compresses the data and processes deletions, they are typically
+	 * significantly smaller than the raw, uncompressed data volume ingested.
+	 * <p>
+	 * IMPORTANT: For Elastic Cloud Serverless, ingest billing is based on the raw,
+	 * uncompressed data volume, not the post-compression metrics reported here. To
+	 * learn more, refer to <a href=
+	 * "https://www.elastic.co/docs/deploy-manage/cloud-organization/billing/elasticsearch-billing-dimensions">Elasticsearch
+	 * billing dimensions</a>.
 	 * <p>
 	 * CAT APIs are only intended for human consumption using the command line or
 	 * Kibana console. They are not intended for use by applications. For
@@ -599,6 +621,17 @@ public class ElasticsearchCatClient extends ApiClient<ElasticsearchTransport, El
 	 * internally to power indexing and search. As a result, all document counts
 	 * include hidden nested documents. To get an accurate count of Elasticsearch
 	 * documents, use the cat count or count APIs.
+	 * <p>
+	 * NOTE: Storage metrics reported by this API reflect the post-compression size
+	 * of the indices on disk. Because these values are calculated after
+	 * Elasticsearch compresses the data and processes deletions, they are typically
+	 * significantly smaller than the raw, uncompressed data volume ingested.
+	 * <p>
+	 * IMPORTANT: For Elastic Cloud Serverless, ingest billing is based on the raw,
+	 * uncompressed data volume, not the post-compression metrics reported here. To
+	 * learn more, refer to <a href=
+	 * "https://www.elastic.co/docs/deploy-manage/cloud-organization/billing/elasticsearch-billing-dimensions">Elasticsearch
+	 * billing dimensions</a>.
 	 * <p>
 	 * CAT APIs are only intended for human consumption using the command line or
 	 * Kibana console. They are not intended for use by applications. For
