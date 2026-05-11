5884bf1b906f23368f4dbf61db6845a32ef21530
Juny Suh <junysuh@gmail.com>
2026-05-11 11:28:18 +0900

    add unit tests for NullSafeJsonGenerator

---
+++ java-client/src/test/java/co/elastic/clients/json/NullSafeJsonGeneratorTest.java
@@ -0,0 +1,106 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package co.elastic.clients.json;
+
+import co.elastic.clients.json.jackson.JacksonJsonProvider;
+import jakarta.json.JsonValue;
+import jakarta.json.spi.JsonProvider;
+import jakarta.json.stream.JsonGenerator;
+import org.junit.jupiter.api.Test;
+
+import java.io.StringWriter;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class NullSafeJsonGeneratorTest {
+
+    private final JsonProvider provider = new JacksonJsonProvider();
+
+    @Test
+    public void testNullReferencesAreWrittenAsJsonNull() {
+        StringWriter writer = new StringWriter();
+        try (JsonGenerator generator = new NullSafeJsonGenerator(provider.createGenerator(writer))) {
+            generator.writeStartObject();
+
+            // writeKey + write(value) overloads with null reference values
+            generator.writeKey("string_field");
+            generator.write((String) null);
+            generator.writeKey("decimal_field");
+            generator.write((BigDecimal) null);
+            generator.writeKey("integer_field");
+            generator.write((BigInteger) null);
+            generator.writeKey("json_value_field");
+            generator.write((JsonValue) null);
+
+            // name+value overloads with null reference values
+            generator.write("named_string", (String) null);
+            generator.write("named_decimal", (BigDecimal) null);
+            generator.write("named_integer", (BigInteger) null);
+            generator.write("named_json_value", (JsonValue) null);
+
+            generator.writeEnd();
+        }
+
+        assertEquals(
+            "{" +
+                "\"string_field\":null," +
+                "\"decimal_field\":null," +
+                "\"integer_field\":null," +
+                "\"json_value_field\":null," +
+                "\"named_string\":null," +
+                "\"named_decimal\":null," +
+                "\"named_integer\":null," +
+                "\"named_json_value\":null" +
+            "}",
+            writer.toString()
+        );
+    }
+
+    @Test
+    public void testNonNullValuesPassThrough() {
+        StringWriter writer = new StringWriter();
+        try (JsonGenerator generator = new NullSafeJsonGenerator(provider.createGenerator(writer))) {
+            generator.writeStartObject();
+            generator.write("string_field", "hello");
+            generator.write("int_field", 42);
+            generator.write("long_field", 10000000000L);
+            generator.write("double_field", 1.5);
+            generator.write("bool_field", true);
+            generator.write("decimal_field", new BigDecimal("3.14"));
+            generator.write("integer_field", new BigInteger("12345"));
+            generator.writeEnd();
+        }
+
+        assertEquals(
+            "{" +
+                "\"string_field\":\"hello\"," +
+                "\"int_field\":42," +
+                "\"long_field\":10000000000," +
+                "\"double_field\":1.5," +
+                "\"bool_field\":true," +
+                "\"decimal_field\":3.14," +
+                "\"integer_field\":12345" +
+            "}",
+            writer.toString()
+        );
+    }
+}
