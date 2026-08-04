1757cd361e14e32d51b43a8073c70d6ca038bce9
Laura Trotta <laura.trotta@elastic.co>
2026-08-04 17:41:32 +0200

    update legacy transport version

--- java-client/build.gradle.kts
+++ java-client/build.gradle.kts
@@ -179,7 +179,7 @@ signing {
 }
 
 dependencies {
-    val elasticsearchVersion = "9.2.0"
+    val elasticsearchVersion = "9.5.0"
     val jacksonVersion = "2.22.0"
     val jackson3Version = "3.1.0"
     val openTelemetryVersion = "1.63.0"
