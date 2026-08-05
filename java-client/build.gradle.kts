6af84892156b9e3e990f186de73e875aba14a88c
Laura Trotta <153528055+l-trotta@users.noreply.github.com>
2026-08-05 12:42:34 +0200

    update jakarta.json-api (#1307)

--- java-client/build.gradle.kts
+++ java-client/build.gradle.kts
@@ -192,7 +192,7 @@ dependencies {
 
     // EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
     // https://github.com/eclipse-ee4j/jsonp
-    api("jakarta.json:jakarta.json-api:2.0.1")
+    api("jakarta.json:jakarta.json-api:2.1.3")
 
     // Needed even if using Jackson to have an implementation of the Jsonp object model
     // EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
